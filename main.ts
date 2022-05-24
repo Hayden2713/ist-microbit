463d2a11459e25061399e4972eecc351ebb3d1dc
RileySykesArden <106127821+RileySykesArden@users.noreply.github.com>
2022-05-24 11:51:21 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,4 @@
 input.onButtonPressed(Button.B, function () {
-    let broadcast_emoji = 0
-    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 5)
-    basic.pause(2000)
-    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 200)
-    basic.pause(5000)
-    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 5)
-    basic.pause(2000)
-    DFRobotMaqueenPlus.mototStop(Motors.ALL)
-    radio.sendString("" + (broadcast_emoji))
+    basic.pause(500)
+    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, input.lightLevel())
 })
