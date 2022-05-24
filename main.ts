a56fd18bc2ca4208c0e6fa5487091cdd0c6e3054
RileySykesArden <106127821+RileySykesArden@users.noreply.github.com>
2022-05-24 11:37:47 +1000

    speed 10 to 5

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     let broadcast_emoji = 0
-    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 10)
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 5)
     basic.pause(2000)
     DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 200)
     basic.pause(5000)
