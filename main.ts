2f2c64c46d8542794b4aeef682cb3c35fa0ae621
Hayden2713 <76269315+Hayden2713@users.noreply.github.com>
2022-05-24 11:33:44 +1000

    Removed LEDlight from Maqueen

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 input.onButtonPressed(Button.A, function () {
     let broadcast_emoji = 0
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
     DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 5)
     basic.pause(2000)
     DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 200)
