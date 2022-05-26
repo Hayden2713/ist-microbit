a59ce28af1b1766a7917ec73b598e5a8b6e10fe0
RileySykesArden <106127821+RileySykesArden@users.noreply.github.com>
2022-05-26 14:53:33 +1000

    rotate on spot bc im bored. + note :)

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
+/**
+ * Riley work
+ * 
+ * (idk what im doing i cant do much until we have the maze
+ */
 input.onButtonPressed(Button.B, function () {
     basic.pause(500)
-    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, input.lightLevel())
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, input.lightLevel())
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, input.lightLevel())
 })
 DFRobotMaqueenPlus.PID(PID.ON)
 soundExpression.yawn.play()
