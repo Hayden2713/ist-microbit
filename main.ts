ce6f1f0ed698c1108f96884959e3bbf911fd63f2
Hayden2713 <76269315+Hayden2713@users.noreply.github.com>
2022-05-26 14:37:18 +1000

    Doing ultrasonic sensor and huskylens stuff

--- main.ts
+++ main.ts
@@ -2,3 +2,5 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(500)
     DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, input.lightLevel())
 })
+DFRobotMaqueenPlus.PID(PID.ON)
+soundExpression.yawn.play()
