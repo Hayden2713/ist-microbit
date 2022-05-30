da17cf122dd7c2a01b196759406780cc80a58644
Hayden2713 <76269315+Hayden2713@users.noreply.github.com>
2022-05-30 12:45:53 +1000

    HuskyLens and Line Tracking Updates

--- main.ts
+++ main.ts
@@ -1,8 +1,40 @@
-input.onButtonPressed(Button.B, function () {
-    basic.pause(500)
-    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, input.lightLevel())
-    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, input.lightLevel())
+input.onButtonPressed(Button.A, function () {
+    huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
+    huskylens.writeName(1, "Hayden")
+    huskylens.writeOSD("Face_Recog", 273, 10)
 })
+input.onButtonPressed(Button.AB, function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    } else {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+            maqueen.motorStop(maqueen.Motors.M1)
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+                maqueen.motorStop(maqueen.Motors.M1)
+            }
+        } else {
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+                maqueen.motorStop(maqueen.Motors.M2)
+                if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+                    maqueen.motorStop(maqueen.Motors.M2)
+                }
+                if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+                    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+                } else {
+                    maqueen.motorStop(maqueen.Motors.M2)
+                }
+            }
+        }
+    }
+})
+input.calibrateCompass()
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
+huskylens.writeName(1, "Hayden")
 DFRobotMaqueenPlus.PID(PID.ON)
 soundExpression.yawn.play()
 basic.forever(function () {
