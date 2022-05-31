e5bde7f3eab71c9c02276007fee482f913489de0
Hayden2713 <76269315+Hayden2713@users.noreply.github.com>
2022-06-01 09:30:38 +1000

    Added Colour Recognition -- **REQUEST FOR TEST OF CODE ON ANOTHER MICRO:BIT**

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+DFRobotMaqueenPlus.I2CInit()
+DFRobotMaqueenPlus.PID(PID.ON)
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
+huskylens.writeName(1, "Green")
+huskylens.writeName(2, "Red")
+huskylens.writeName(3, "Purple")
 basic.forever(function () {
 	
 })
